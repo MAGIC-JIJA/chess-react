4bd89e2d6fe8fb3a510968aeb3bb2c83959a3047
ubozhykovgeorge <snake.smail.com@gmail.com>
2024-04-15 11:37:52 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import "./App.css"
+import BoardComponent from "./componets/BoardComponent";
+import {Board} from "./models/Board";
+import {Player} from "./models/Player";
+import {Colors} from "./models/Colors";
+import LostFiguresComponent from "./componets/LostFiguresComponent";
+import Timer from "./componets/Timer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function LostFigures() {
+    return null;
 }
+
+const App = () => {
+    const [board, setBoard] = useState(new Board())
+    const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+    const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+    const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
+    useEffect(() => {
+        restart()
+        setCurrentPlayer(whitePlayer);
+    },[])
+
+    function restart(){
+        const newBoard = new Board();
+        newBoard.initeCells();
+        newBoard.addFigures();
+        setBoard(newBoard);
+    }
+
+    function swapPlayer(){
+        setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+    }
+
+  return (
+      <div className="app">
+          <Timer
+              restart={restart}
+              currentPlayer={currentPlayer}
+          />
+        <BoardComponent
+        board={board}
+        setBoard={setBoard}
+        currentPlayer ={currentPlayer}
+        swapPlayer={swapPlayer}
+        />
+          <div>
+              <LostFiguresComponent
+              title="Black Figures"
+              figures={board.lostBlackFigures}
+              />
+              <LostFiguresComponent
+                  title="White Figures"
+                  figures={board.lostWhiteFigures}
+              />
+          </div>
+      </div>
+  );
+};
 
 export default App;
